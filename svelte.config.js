1537aa2ee1284a0e947b925768b0a2a2cd527ac8
Kevin Kern <65562996+kkern42@users.noreply.github.com>
2022-10-24 13:32:40 -0400

    adding netlify configurations

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		adapter: adapter()
+	  adapter: adapter({
+		edge: false,
+		split: true
+	  })
 	}
-};
-
-export default config;
+  };
