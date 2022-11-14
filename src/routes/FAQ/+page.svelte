ee1a50e01fd44dcc1b399eaefe50ba136031290b
ashleytkim <57121181+ashleytkim@users.noreply.github.com>
2022-11-14 16:18:50 -0500

    added faq accordion

--- src/routes/FAQ/+page.svelte
+++ src/routes/FAQ/+page.svelte
@@ -4,16 +4,43 @@
 </svelte:head>
 
 <script>
+	import { Accordion, AccordionItem } from 'svelte-collapsible'
 	
+	let questions = [
+		{key:"1", 
+		question: "How do I join a tournament?",
+		answer: "Look out for an email in January or reach out to Ashley Kim on teams."},
+
+		{key:"2",
+		question: "What tier should I be in?",
+		answer: "Some answer"}
+	]
+
+
+
 </script>
 
-<div class="FAQ">
-	helloe dudes
-</div>
+<Accordion>
+	{#each questions as question}
+	<AccordionItem key={question.key}>
+		<div slot='header' class='header'>
+			<!-- <img src={item.img} alt="Cover"/> -->
+			<div>
+				<p>{ question.question }</p>
+			</div>
+		</div>
+		
+		<p slot='body' class='body'>
+			{ question.answer }
+		</p>
+	</AccordionItem>
+	
+  {/each}
+</Accordion>
 
 <style>
 	
-	.FAQ{
+	:global(.accordion){
 		margin: 0 auto;
 		padding-right: 6rem;
 		padding-left: 6rem;
@@ -22,6 +49,27 @@
       	text-align: center;
     }
 
+	:global(.accordion-item) {
+		margin: 1rem; 
+		border-bottom: 1px solid rgb(100, 120, 140);
+	}
 
+	.header {
+		display: flex;
+		align-items: center;
+		padding: 0.5em;
+	}
+	
+	
+	.header p {
+		font-size: 18px;
+		margin: 0;
+	}
+	
+	.body {
+		padding: 0.5em;
+		margin: 0;
+		font-size: 18px;
+	}
 		
-	</style>
+</style>
