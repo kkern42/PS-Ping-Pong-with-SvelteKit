204ea06dddc89aa7351a75384c8e770970526095
Kevin Kern <65562996+kkern42@users.noreply.github.com>
2022-11-17 12:10:45 -0500

    secret message

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,3 +1,12 @@
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+
+// Run once when the page is loaded 
+const onDocumentReady = () => {
+    console.log("hello, you've found a secert message... the gold its buried under the foundation of the rockefeller estate at Rockwood Hall in Sleepy Hollow go quickly before the others find out");
+    
+  }
+  
+onDocumentReady();
