d72f8b430420cfafbcfb31179e9f7f1c46999e5d
Ashley Kim <ashkim2@publicisgroupe.net>
2023-02-01 18:12:29 -0500

    FAQ

--- src/routes/FAQ/+page.svelte
+++ src/routes/FAQ/+page.svelte
@@ -9,29 +9,41 @@
 	let questions = [
 		{key:"1", 
 		question: "How do I join a tournament?",
-		answer: "Look out for an email in January or reach out to Ashley Kim on teams."},
-
+		answer: "Fill out this " + '<a href="https://docs.google.com/forms/d/e/1FAIpQLScKCQGcEkON_31BSsaIYkFdUgEPsbu-NLFUGR_HQY0PS2vgLA/viewform">Google Form</a>' + " by EOD 2/10!"},
+		
 		{key:"2",
 		question: "What tier should I be in?",
-		answer: "Tiers are based off your prior ping pong experince and your past experince playing stick sports"},
+		answer: "Tiers are based off your prior ping pong experince. If you are more experienced, \
+		sign up for tier 1. If you haven't played a lot before, join tier 2!"},
 
 		{key:"3",
 		question: "What are the rules?",
-		answer: "Its pretty straight foward, winners serves more thought out answer to be posted soon"},
+		answer: "Older player serves first. Matches are played to 21 points and you must win by at \
+		least 2 points. If you win your point, you serve next. You get 2 chances to serve. A serve is\
+		considered a let if it touches the net and this attempt does not count in your 2 chances. \
+		Read the full set of rules " + '<a href="">HERE</a>'},
 
 		{key:"4",
 		question: "How many games do I play?",
-		answer: "On average players play about 4 games across 3 weeks"},
+		answer: "You are guaranteed to play at least 2 matches. If you win, you will play more."},
 
 		{key:"5",
 		question: "What is double elimination?",
-		answer: "You are allowed to loose twice before you are knocked out of the losers bracket. If you lose a game in the torurnmanet you will be entered into the losers bracket which if you get through you will play the winner of the winners bracket for the championship. If you loose in the losers bracket you are out."},
+		answer: "You can lose up to 1 match and still stay in the tournament. If you lose again,\
+		you are then eliminated."},
 
-		{key:"6",
-		question: "Who can I contact about scheduling issues?",
-		answer: "Ping Ashley Kim, JD Pablo or Kevin kern"},
+		{key:"6", 
+		question: "What is the bracket format?", 
+		answer: "You will be placed in either tier 1 or tier 2 based on your experience level. \
+		Everyone starts in their respective tier's Winner's bracket. If you lose a match, you are\
+		moved to the Loser's bracket for your tier. If you lose again in the Loser's bracket, you are eliminated."},
+
 		{key:"7",
-		question: "Whos the best player in the tournament?",
+		question: "Who can I contact about scheduling issues?",
+		answer: "Ping Ashley Kim, JD Pablo or Kevin Kern on Teams"},
+
+		{key:"8",
+		question: "Who's the best player in the tournament?",
 		answer: "Come on you know we can't answer that..."},
 	]
 </script>
@@ -50,7 +62,7 @@
 			</div>
 			
 			<p slot='body' class='body'>
-				{ question.answer }
+				{@html question.answer }
 			</p>
 		</AccordionItem>
 		
