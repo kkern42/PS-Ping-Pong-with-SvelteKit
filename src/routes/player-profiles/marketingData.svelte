88fe4a80002b5f9e207d1e5d5601a2db85f2a7f3
Ashley Kim <ashkim2@publicisgroupe.net>
2023-03-03 13:32:03 -0500

    updated profiles

---
+++ src/routes/player-profiles/marketingData.svelte
@@ -0,0 +1,37 @@
+<script context="module">
+
+    export let marketing = [
+        {
+            name: 'Rufino Balayo',
+			grip: 'Handshake',
+			record: '0-0',
+			strength: 'HOW DO I INSERT AN EMOJI',
+			weakness: '',
+			picture: 'https://firebasestorage.googleapis.com/v0/b/pspp-e8218.appspot.com/o/profile-images%2Frufino.jpeg?alt=media&token=e79ee215-14c0-46a2-ab5d-72fb5169bfcc',
+			reverse: '',
+			margin: ''
+        },
+        {
+            name: 'Elias Eberman',
+			grip: 'Handshake',
+			record: '0-0',
+			strength: 'Making it into tier 1 for his debut tournament',
+			weakness: 'Having to listen to Kevin talk while playing',
+			picture: 'https://firebasestorage.googleapis.com/v0/b/pspp-e8218.appspot.com/o/profile-images%2Felias.jpeg?alt=media&token=348bff10-41a8-410a-bd0a-7bd365897e73',
+			reverse: '',
+			margin: ''
+        },
+        {
+            name: 'Grace Moon',
+			grip: 'Handshake',
+			record: '0-0',
+			strength: 'Quick to network, make friends and join the tournament',
+			weakness: 'Only has 15 minutes to play',
+			picture: 'https://firebasestorage.googleapis.com/v0/b/pspp-e8218.appspot.com/o/profile-images%2FGrace.jpeg?alt=media&token=7b549b13-7443-46c2-96ad-06ac2d8d38ce',
+			reverse: '',
+			margin: ''
+        }
+    ]
+
+
+</script>
